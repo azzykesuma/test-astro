4f3a30d52af7db7fd79096c73011c628db625bd6
Azzzy <official.azzy@gmail.com>
2025-06-11 17:25:27 +0700

    test build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
 
   integrations: [react()],
 });
